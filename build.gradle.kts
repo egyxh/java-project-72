a91bcddf2eab24d0da6137c9e2c6d07e856ef109
egor_semichev <thebestists@gmail.com>
2025-11-21 14:09:42 +0300

    add fat jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,9 +29,29 @@ java {
     targetCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.jar {
-    manifest {
-        attributes("Main-Class" to "hexlet.code.App")
+tasks {
+    jar {
+        manifest {
+            attributes("Main-Class" to application.mainClass.get())
+        }
+    }
+
+    val fatJar = register<Jar>("fatJar") {
+        dependsOn.addAll(listOf("compileJava", "processResources"))
+        archiveClassifier.set("standalone")
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+        manifest {
+            attributes("Main-Class" to application.mainClass.get())
+        }
+        val sourcesMain = sourceSets.main.get()
+        val contents = configurations.runtimeClasspath.get()
+            .map { if (it.isDirectory) it else zipTree(it) } +
+                sourcesMain.output
+        from(contents)
+    }
+
+    build {
+        dependsOn(fatJar)
     }
 }
 
